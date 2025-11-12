c23747d83e7d75dba7d525daf2cc7bb174490187
Grongasx <gustavinihb12@gmail.com>
2025-11-12 19:14:17 -0300

    O commit mais Bonito do projeto

--- Pages/Produto/Cardapio.cshtml.cs
+++ Pages/Produto/Cardapio.cshtml.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using saborGregoNew.Repository;
-using SaborGregoNew.Models; // Adicionar using para Models.Produto
 
 namespace SaborGregoNew.Pages
 {
